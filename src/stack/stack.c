404f990e2825878440263b3169b9a5e387c82b4c
Kaiqin Huang <huang214@mail.chapman.edu>
2016-11-14 00:55:58 +0000

    edit

--- src/stack/stack.c
+++ src/stack/stack.c
@@ -2,20 +2,23 @@
 #include <stdlib.h>
 #include "stack.h"
 
-//define a new NULL stack and return.
+// Define a new NULL stack pointer and return it
+// Constructor that initializes an empty stack pointer
 STACK new_stack() {
     STACK new = NULL;
     return new;
 }
 
-/*
-push one value into the stack
-1. allocate memory for pushing value,type is STACK;
-2. set the value of the new stack with val;
-3. set the head of the new stack with pointer s, which means s is new's head.
-4. reset the address of with new, which means move s to the new pushed stack.
-*/
-void push_stack(STACK *s, SVALUE val) {
+/* Add one more element
+ * Setter for pushing a value onto the stack
+    1. Allocate memory for a new stack and return the address of the new stack (what function malloc() does);
+    2. Set the value in the new stack to "val";
+    3. Set the head of the new stack to s;
+       It means the new stack is now pointing to the next stack, which was the first stack before;
+    4. Reset address s to address new, which means s is pointing to the newly pushed stack.
+ */
+void push_stack(STACK *s, SVALUE val) { 
+    // Use STACK *s instead of just STACK s because we do want to change the address in pointer s, not the address in the copy of s
     STACK new = (STACK) malloc(sizeof(struct stack));
     new->data = val;
     new->head = *s;
@@ -23,12 +26,18 @@ void push_stack(STACK *s, SVALUE val) {
 }
 
 /*
-pop one value from stack
-1. define temporay variable val and temp STACK;
-2. if s point to NULL, set val with -1;
-3. otherwise, set data that s pointed right now to val, set temp with the address of s, then set the new address of s with its header address, free the memory that temp point.
-*/
+ * Remove the first element and return the value in the removed stack
+ * Getter for popping a value off the stack
+    1. Define temporay variable SVALUE (int) val and STACK temp;
+    2. Check whether s points to NULL.  If yes, it means s is already the head in the very bottom stack.  Set "val" to -1;
+    3. Otherwise, set "val" equal to the data value in the first stack that we are trying to remove now;
+    4. Set "temp" to address s, which is like making a copy of address s;
+    4. Set the address in s to the head address first stack, so now s is pointing to the second stack;
+    5. Free the memory of the address in temp, which is the first stack;
+    6. Return the value in the deleted stack.
+ */
 SVALUE pop_stack(STACK *s) {
+    // Use STACK *s instead of just STACK s for the same reason in push_stack
     SVALUE val;
     STACK temp;
     if (*s == NULL) {
@@ -44,24 +53,33 @@ SVALUE pop_stack(STACK *s) {
 }
 
 // Printer for svalue
-void print_svalue(SVALUE val){
-	printf("%d ", val);
+void print_svalue(SVALUE val) {
+	printf("%d", val);
 }
-
 
 /* Printer for stack
-print each element from upper to buttom
+ * Print each element from up to bottom
 */
-void print_stack(STACK s){
-	STACK temp;
-	temp=s;
-
-	while (s->head != NULL){
-		print_svalue((s->data));
-		s=s->head;
+void print_stack(STACK s) {
+	while (s->head != NULL) {
+		print_svalue(s->data);
+        printf("\t");
+		s = s->head;
 	}
-	print_svalue((s->data));
+	print_svalue(s->data);
 	printf("\n");
-
-	s=temp;
 }
+
+/* Slightly different, but also works
+ * Printer for stack
+ * No need to do "print_svalue(s->data)" again after while loop, but would leave an unused tab at the end of the line
+
+void print_stack(STACK s) {
+	while (s != NULL) {
+		print_svalue(s->data);
+        printf("\t");
+		s = s->head;
+	}
+	puts("");
+}
+*/
